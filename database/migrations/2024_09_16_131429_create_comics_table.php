98db7da7ce67005fed2eb34e96d20fe4cddf64da
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-09-16 15:51:50 +0200

    feat: create table and model

---
+++ database/migrations/2024_09_16_131429_create_comics_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->string('slug')->unique();
+            $table->text('description')->nullable();
+            $table->string('thumb');
+            $table->string('price');
+            $table->string('series', 100);
+            $table->date('sale_date');
+            $table->string('type', 30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comics');
+    }
+};
