b32147e41caccc6b04cc7f84f12cd45099a4b5a5
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-09-17 10:15:25 +0200

    feat: add static home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,11 @@
 
 @section('content')
 
+<div class="container my-5">
+    <h1>HOME</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem harum at voluptatum voluptatibus maiores! Maxime in perferendis tempora modi odit maiores adipisci, ex ab dolor quam hic numquam perspiciatis quae vel dignissimos dolorem aliquam voluptatum sequi minima porro. Animi fugiat explicabo minima. Fuga, molestiae nisi et ex saepe facere maiores placeat asperiores ducimus architecto quidem esse laborum explicabo accusantium eos expedita? Inventore delectus quia nostrum minus deleniti odio. Optio, voluptas a aspernatur obcaecati vitae quis tempore adipisci commodi eos totam fugiat nobis necessitatibus inventore esse non temporibus odit at placeat ut libero deserunt? Accusantium, cupiditate error aliquam soluta deserunt quasi.</p>
+</div>
+
 @endsection
 
 
