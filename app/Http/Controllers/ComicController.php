bf66c6149db0d5f5b217cf753927901065090bfa
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-09-18 17:03:09 +0200

    feat: add form request validation

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Functions\Helper;
+use App\Http\Requests\ComicRequest;
 use Illuminate\Http\Request;
 use App\Models\Comic;
 
@@ -28,31 +29,8 @@ class ComicController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(ComicRequest $request)
     {
-        $request->validate([
-            'title' => 'required|min:3|max:100',
-            'price' => 'required|min:2',
-            'series' => 'required|min:3|max:100',
-            'sale_date' => 'required|min:10',
-            'type' => 'required|min:3|max:30'
-        ],
-        [
-            'title.required' => 'Il Titolo è obbligatorio',
-            'title.min' => 'Il Titolo deve avere almeno :min caratteri',
-            'title.max' => 'Il Titolo deve avere massimo :max caratteri',
-            'price.required' => 'Il Prezzo è obbligatorio',
-            'price.min' => 'Il Prezzo deve avere almeno :min caratteri',
-            'series.required' => 'La Serie è obbligatoria',
-            'series.min' => 'La Serie deve avere almeno :min caratteri',
-            'series.min' => 'La Serie deve avere massimo :max caratteri',
-            'sale_date.required' => 'La Data di Vendita è obbligatoria',
-            'sale_date.min' => 'La Data di Vendita deve avere almeno :min caratteri',
-            'type.required' => 'Il Tipo è obbligatorio',
-            'type.min' => 'Il Tipo deve avere almeno :min caratteri',
-            'type.max' => 'Il Tipo deve avere massimo :max caratteri'
-        ]);
-
         $data = $request->all();
         $data['slug'] = Helper::generateSlug($data['title'], Comic::class);
 
