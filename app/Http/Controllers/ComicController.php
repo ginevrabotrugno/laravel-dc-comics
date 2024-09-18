6a8836d21146ee01f155cc1faa473f7679108010
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-09-18 16:57:53 +0200

    feat: add validation to create

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -30,12 +30,33 @@ class ComicController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'title' => 'required|min:3|max:100',
+            'price' => 'required|min:2',
+            'series' => 'required|min:3|max:100',
+            'sale_date' => 'required|min:10',
+            'type' => 'required|min:3|max:30'
+        ],
+        [
+            'title.required' => 'Il Titolo è obbligatorio',
+            'title.min' => 'Il Titolo deve avere almeno :min caratteri',
+            'title.max' => 'Il Titolo deve avere massimo :max caratteri',
+            'price.required' => 'Il Prezzo è obbligatorio',
+            'price.min' => 'Il Prezzo deve avere almeno :min caratteri',
+            'series.required' => 'La Serie è obbligatoria',
+            'series.min' => 'La Serie deve avere almeno :min caratteri',
+            'series.min' => 'La Serie deve avere massimo :max caratteri',
+            'sale_date.required' => 'La Data di Vendita è obbligatoria',
+            'sale_date.min' => 'La Data di Vendita deve avere almeno :min caratteri',
+            'type.required' => 'Il Tipo è obbligatorio',
+            'type.min' => 'Il Tipo deve avere almeno :min caratteri',
+            'type.max' => 'Il Tipo deve avere massimo :max caratteri'
+        ]);
+
         $data = $request->all();
-
-        $new_comic = new Comic();
-
         $data['slug'] = Helper::generateSlug($data['title'], Comic::class);
 
+        $new_comic = new Comic();
         $new_comic->fill($data);
 
         $new_comic->save();
--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -5,31 +5,57 @@
     <div class="container my-5">
         <h1 class="my-3">  ADD NEW COMIC </h1>
 
+        @if ($errors->any())
+            <div class="alert alert-danger" role="alert">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{$error}}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+
         <form action="{{route('comics.store')}}" method="POST">
             @csrf
             <div class="mb-3">
                 <label for="title" class="form-label">Titolo</label>
-                <input name="title" type="text" class="form-control" id="title">
+                <input name="title" type="text" class="form-control @error('title') is-invalid @enderror" id="title" value="{{old('title')}}">
+                @error('title')
+                    <small class="text-danger">{{$message}}</small>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="thumb" class="form-label">Path Immagine</label>
-                <input name="thumb" type="text" class="form-control" id="thumb">
+                <input name="thumb" type="text" class="form-control" id="thumb" value="{{old('thumb')}}">
             </div>
             <div class="mb-3">
                 <label for="price" class="form-label">Prezzo</label>
-                <input name="price" type="text" class="form-control" id="price">
+                <input name="price" type="text" class="form-control @error('price') is-invalid @enderror" id="price" value="{{old('price')}}">
+                @error('price')
+                    <small class="text-danger">{{$message}}</small>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="series" class="form-label">Serie</label>
-                <input name="series" type="text" class="form-control" id="series">
+                <input name="series" type="text" class="form-control @error('series') is-invalid @enderror" id="series" value="{{old('series')}}">
+                @error('series')
+                    <small class="text-danger">{{$message}}</small>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="sale_date" class="form-label">Data di vendita</label>
-                <input name="sale_date" type="text" class="form-control" id="sale_date" placeholder="YYYY-MM-DD">
+                <input name="sale_date" type="text" class="form-control @error('sale_date') is-invalid @enderror" id="sale_date" value="{{old('sale_date')}}" placeholder="YYYY-MM-DD">
+                @error('sale_date')
+                    <small class="text-danger">{{$message}}</small>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="type" class="form-label">Tipo</label>
-                <input name="type" type="text" class="form-control" id="type">
+                <input name="type" type="text" class="form-control @error('type') is-invalid @enderror" id="type" value="{{old('type')}}">
+                @error('type')
+                    <small class="text-danger">{{$message}}</small>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="description" class="form-label">Descrizione</label>
